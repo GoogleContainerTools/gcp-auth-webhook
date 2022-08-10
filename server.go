7440adc4f45931c540b7cad7b882541e5b2ef57a
Steven Powell <spowellgrr@gmail.com>
2022-08-10 16:57:19 -0700

    clean up error logging

--- server.go
+++ server.go
@@ -312,7 +312,7 @@ func needsEnvVar(c corev1.Container, name string) bool {
 	return true
 }
 
-func updateCheck() error {
+func updateCheck() {
 	type release struct {
 		Name string `json:"name"`
 	}
@@ -321,41 +321,35 @@ func updateCheck() error {
 
 	resp, err := http.Get("https://storage.googleapis.com/minikube-gcp-auth/releases.json")
 	if err != nil {
-		return fmt.Errorf("failed to get releases file: %v", err)
+		log.Printf("failed to get releases file: %v", err)
 	}
 	defer resp.Body.Close()
 	if err := json.NewDecoder(resp.Body).Decode(&releases); err != nil {
-		return fmt.Errorf("failed to decode releases file: %v", err)
+		log.Printf("failed to decode releases file: %v", err)
 	}
 	if len(releases) == 0 {
-		return fmt.Errorf("no releases found in releases file")
+		log.Printf("no releases found in releases file")
 	}
 
 	currVersion, err := semver.ParseTolerant(Version)
 	if err != nil {
-		return fmt.Errorf("unable to parse current version: %v", err)
+		log.Printf("unable to parse current version: %v", err)
 	}
 	name := releases[0].Name
 	latestVersion, err := semver.ParseTolerant(name)
 	if err != nil {
-		return fmt.Errorf("unable to parse latest version: %v", err)
+		log.Printf("unable to parse latest version: %v", err)
 	}
 
 	if currVersion.LT(latestVersion) {
 		log.Printf("gcp-auth-webhook %s is available!", name)
 	}
-
-	return nil
 }
 
 func updateTicker() {
-	if err := updateCheck(); err != nil {
-		log.Println(err)
-	}
+	updateCheck()
 	for range time.Tick(12 * time.Hour) {
-		if err := updateCheck(); err != nil {
-			log.Println(err)
-		}
+		updateCheck()
 	}
 }
 
